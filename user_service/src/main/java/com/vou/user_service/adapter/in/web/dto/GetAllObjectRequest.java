5cabce7080b47723bb5e4fd840aaf8bfd90bdb08
HTNam1008 <dev.hoangthanhnam.1008@gmail.com>
2025-01-10 14:28:27 +0700

    fea: endpoint get all user based on role

---
+++ user_service/src/main/java/com/vou/user_service/adapter/in/web/dto/GetAllObjectRequest.java
@@ -0,0 +1,16 @@
+package com.vou.user_service.adapter.in.web.dto;
+
+public class GetAllObjectRequest {
+    private String role;
+
+    public GetAllObjectRequest() {
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ user_service/src/main/java/com/vou/user_service/application/service/strategy/get_all_object_strategy/GetAllBrandStrategy.java
@@ -0,0 +1,21 @@
+package com.vou.user_service.application.service.strategy.get_all_object_strategy;
+
+import com.vou.user_service.adapter.out.persistence.BrandRepository;
+import com.vou.user_service.domain.model.Brand;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GetAllBrandStrategy implements GetAllObjectStrategy {
+    private final BrandRepository brandRepository;
+
+    public GetAllBrandStrategy(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
+    @Override
+    public List<Brand> getAllObject() {
+        return brandRepository.findAll();
+    }
+}
